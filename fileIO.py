64aed6d34b2e288f5ba6f9ae20b093f357fd816a
jaclynchorton <jaclynchorton@gmail.com>
2019-12-06 00:57:11 -0800

    Commited individual files and OrganizeMe folder

---
+++ fileIO.py
@@ -0,0 +1,13 @@
+f = open('inputFile.txt', 'r')
+passfile = open('PassFile.txt', 'w')
+failfile = open('FailFile.txt', 'w')
+for line in f:
+    line_split = line.split()
+    if line_split[2] == 'P':
+        passfile.write(line)
+ 
+    else:
+        failfile.write(line)
+f.close()
+passfile.close()
+failfile.close()
